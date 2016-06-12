30126986cbce29870174b89d0fe8e01beb0c73cf
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-06-12 09:54:37 -0500

    case _

--- end.py
+++ end.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 def checkPhoneNode(phone,name):
 	phone,name = str(phone), str(name)
+	if '_' in name: name = name.replace('_',' ')
 	if neoCon.nodeExists(phone,key='phone'): return neoCon.getNode(phone,key='phone')	
 	else:           return neoCon.createNode({'name':name,'phone':phone},labels=['person'])
 
